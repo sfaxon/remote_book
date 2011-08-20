381e01123dd66c758fce05afcb4ad9fbd40cf110
Seth Faxon <seth.faxon@gmail.com>
2011-08-19 22:04:47 -0700

    fix spec description

--- spec/lib/amazon_spec.rb
+++ spec/lib/amazon_spec.rb
@@ -7,7 +7,7 @@ describe RemoteBook::Amazon do
                                          :key_id => 'bar',
                                          :secret_key => 'baz'}
   end
-  it "should return nil when setting doesn't exist" do
+  it "should set large_image on successful get" do
     FakeWeb.register_uri(:get, %r|http://ecs\.amazonaws\.com/(.*)|, :body => load_file("amazon_1433506254.xml"))
     a = RemoteBook::Amazon.find_by_isbn("1433506254")
     a.large_image.should == "http://ecx.images-amazon.com/images/I/41xMfBAsMnL.jpg"
